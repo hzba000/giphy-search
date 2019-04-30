c732df9061f6269aaa7f37180b80d478c5917f7f
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2019-04-30 06:00:38 -0700

    Updates

---
+++ precache-manifest.a8d1046d0ef290927eadd80ea90e7c73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "456d949a186663b15f39257ce8186c76",
+    "url": "/giphy-search/index.html"
+  },
+  {
+    "revision": "385d9c79dce130fda184",
+    "url": "/giphy-search/static/css/main.658b0201.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/giphy-search/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "385d9c79dce130fda184",
+    "url": "/giphy-search/static/js/main.c0bf9df2.chunk.js"
+  },
+  {
+    "revision": "d768df5d6a67a3d021bd",
+    "url": "/giphy-search/static/js/runtime~main.8080b567.js"
+  }
+]);
